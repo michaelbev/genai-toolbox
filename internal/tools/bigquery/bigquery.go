3cea112a78d3543c9f9d33dcab91139493243f05
duwenxin <duwenxin@google.com>
2025-06-17 16:52:22 -0400

    reduce to helper

--- internal/tools/bigquery/bigquery.go
+++ internal/tools/bigquery/bigquery.go
@@ -127,56 +127,19 @@ func (t Tool) Invoke(ctx context.Context, params tools.ParamValues) ([]any, erro
 		// BigQuery's QueryParameter only accepts typed slices as input
 		// This checks if the param is an array.
 		// If yes, convert []any to typed slice (e.g []string, []int)
-		switch vType := value.(type) {
+		switch arrayParam := value.(type) {
 		case []any:
 			var itemType string
+			var err error
 			for _, p := range t.Parameters {
 				// iterate through parameters to get array's item type
 				if name == p.GetName() {
 					itemType = p.McpManifest().Items.Type
 				}
 			}
-			switch itemType {
-			case "string":
-				typedSlice := make([]string, len(vType))
-				for j, item := range value.([]any) {
-					if s, ok := item.(string); ok {
-						typedSlice[j] = s
-					} else {
-						return nil, fmt.Errorf("parameter '%s': expected item at index %d to be string, got %T", name, j, item)
-					}
-				}
-				value = typedSlice
-			case "integer":
-				typedSlice := make([]int64, len(vType))
-				for j, item := range value.([]any) {
-					if i, ok := item.(int); ok {
-						typedSlice[j] = int64(i)
-					} else {
-						return nil, fmt.Errorf("parameter '%s': expected item at index %d to be integer, got %T", name, j, item)
-					}
-				}
-				value = typedSlice
-			case "float":
-				typedSlice := make([]float64, len(vType))
-				for j, item := range value.([]any) {
-					if f, ok := item.(float64); ok {
-						typedSlice[j] = f
-					} else {
-						return nil, fmt.Errorf("parameter '%s': expected item at index %d to be float, got %T", name, j, item)
-					}
-				}
-				value = typedSlice
-			case "boolean":
-				typedSlice := make([]bool, len(vType))
-				for j, item := range value.([]any) {
-					if b, ok := item.(bool); ok {
-						typedSlice[j] = b
-					} else {
-						return nil, fmt.Errorf("parameter '%s': expected item at index %d to be boolean, got %T", name, j, item)
-					}
-				}
-				value = typedSlice
+			value, err = convertAnySliceToTyped(arrayParam, itemType, name)
+			if err != nil {
+				return nil, fmt.Errorf("unable to convert []any to typed slice: %w", err)
 			}
 		}
 
@@ -235,4 +198,48 @@ func (t Tool) McpManifest() tools.McpManifest {
 
 func (t Tool) Authorized(verifiedAuthServices []string) bool {
 	return tools.IsAuthorized(t.AuthRequired, verifiedAuthServices)
+}
+
+func convertAnySliceToTyped(s []any, itemType, paramName string) (any, error) {
+	var typedSlice any
+	switch itemType {
+	case "string":
+		typedSlice := make([]string, len(s))
+		for j, item := range s {
+			if s, ok := item.(string); ok {
+				typedSlice[j] = s
+			} else {
+				return nil, fmt.Errorf("parameter '%s': expected item at index %d to be string, got %T", paramName, j, item)
+			}
+		}
+	case "integer":
+		typedSlice := make([]int64, len(s))
+		for j, item := range s {
+			if i, ok := item.(int); ok {
+				typedSlice[j] = int64(i)
+			} else {
+				return nil, fmt.Errorf("parameter '%s': expected item at index %d to be integer, got %T", paramName, j, item)
+			}
+		}
+	case "float":
+		typedSlice := make([]float64, len(s))
+		for j, item := range s {
+			if f, ok := item.(float64); ok {
+				typedSlice[j] = f
+			} else {
+				return nil, fmt.Errorf("parameter '%s': expected item at index %d to be float, got %T", paramName, j, item)
+			}
+		}
+	case "boolean":
+		typedSlice := make([]bool, len(s))
+		for j, item := range s {
+			if b, ok := item.(bool); ok {
+				typedSlice[j] = b
+			} else {
+				return nil, fmt.Errorf("parameter '%s': expected item at index %d to be boolean, got %T", paramName, j, item)
+			}
+		}
+
+	}
+	return typedSlice, nil
 }
